86ded91956a899fa90d041f1afbc2671838f9893
disflyer <disflyer123@gmail.com>
2023-03-03 15:18:25 +0800

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -27,7 +27,7 @@ export default async function (req, res) {
 
   try {
     const completion = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: "gpt-3.5-turbo",
       prompt: generatePrompt(animal),
       temperature: 0.6,
     });
@@ -49,14 +49,6 @@ export default async function (req, res) {
 }
 
 function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+  const capitalizedAnimal = animal
+  return `${capitalizedAnimal}`;
 }
