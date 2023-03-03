eb2678f9d0b0bf24a01740c13aee10942e4be4ad
disflyer <disflyer123@gmail.com>
2023-03-03 15:55:17 +0800

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -28,7 +28,7 @@ export default async function (req, res) {
   try {
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [{"role": "user", "content": animal }]
+      messages: [{"role": "user", "content": animal }],
       temperature: 0.6,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
