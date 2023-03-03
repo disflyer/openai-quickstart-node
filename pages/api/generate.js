516cf33c6710140e24009a01f3d662421453652b
disflyer <disflyer123@gmail.com>
2023-03-03 15:52:22 +0800

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -28,7 +28,7 @@ export default async function (req, res) {
   try {
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      prompt: generatePrompt(animal),
+      messages: [{"role": "user", "content": animal }]
       temperature: 0.6,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
@@ -46,9 +46,4 @@ export default async function (req, res) {
       });
     }
   }
-}
-
-function generatePrompt(animal) {
-  const capitalizedAnimal = animal
-  return `${capitalizedAnimal}`;
 }
